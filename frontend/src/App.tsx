010b4e2b2c99caaa1008af8f8e1766899176d954
orkarfabianthewise <orkarmelch@gmail.com>
2025-02-26 23:10:16 +0100

    wallet connect custom button set

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WalletContextProvider from "./components/WalletConnect";
 
 const App: React.FC = () => {
   return (
